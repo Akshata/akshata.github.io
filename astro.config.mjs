5b2a233d2d5f2a8c569a057b1bcc29c64e568591
Akshata <akshata0303@gmail.com>
2025-05-07 22:36:34 +0530

    change the title

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://aksahta.github.io',
+	
 	integrations: [
 		starlight({
 			title: 'My Docs',
